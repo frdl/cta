d9e89e9c8e2e197b8e2b38974a5a5d5325eb58a0
Till Wehowski <github@webfan.de>
2022-08-27 17:46:26 +0200

    Update HashTypeInterface.php

--- src/HashTypeInterface.php
+++ src/HashTypeInterface.php
@@ -1,7 +1,7 @@
 <?php
 
 interface HashTypeInterface {
-    public function getType() : string;
+    public function getType() : array;
     public function hash($contents) : array;
     public function __toString() : string;
 }
