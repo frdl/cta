b249c34eded470ee8e967e718068c79fc450d3ca
Till Wehowski <github@webfan.de>
2022-08-27 18:40:39 +0200

    Update XHashSha1.php

--- src/XHashSha1.php
+++ src/XHashSha1.php
@@ -14,12 +14,12 @@ class XHashSha1 implements HashTypeInterface
     public function getHash(){
         return $this->hash;
     }
-    public function getType() {
+    public function getType(): array {
         return [          
            '1.3.6.1.4.1.37553.8.1.8.1.16606.1.56234465',
         ];
-    }: array;
-    public function __invoke($contents){
+    };
+    public function __invoke($contents): array{
          if(!is_string($contents)){      
            if(null === $this->getHash()){          
              throw new \Exception('You must hash some contents before you can get a hash!');       
@@ -32,11 +32,11 @@ class XHashSha1 implements HashTypeInterface
             strlen($contents),
          ];
        return $this->hash;
-    }: array;
-    public function __toString(){
+    };
+    public function __toString(): string{
        if(null === $this->getHash()){
           throw new \Exception('You must hash some contents before you can get a string!'); 
        }
        return implode($this->sep, array_reverse($this->hash));
-    }: string; 
+    }; 
 }
