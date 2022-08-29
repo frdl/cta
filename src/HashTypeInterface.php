824fe36bf73e11c41f96a6191ad3d1e588e3def0
Till Wehowski <github@webfan.de>
2022-08-29 10:56:54 +0200

    Update HashTypeInterface.php

--- src/HashTypeInterface.php
+++ src/HashTypeInterface.php
@@ -3,6 +3,6 @@ namespace Webfan\cta\HashType;
 
 interface HashTypeInterface {
     public function getType() : array;
-    public function __invoke($contents) : array;
+    public function __invoke(string $contents = null) : array;
     public function __toString() : string;
 }
