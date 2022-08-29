c7622bb5b8d8217c569ff348ec5d69786d38e334
Till Wehowski <github@webfan.de>
2022-08-29 11:53:38 +0200

    Update XHashSha1.php

--- src/XHashSha1.php
+++ src/XHashSha1.php
@@ -5,12 +5,19 @@ namespace Webfan\cta\HashType;
 class XHashSha1 implements HashTypeInterface
 {
     protected $hash = null;
-    public $sep = '/';
-    public function __construct(string $contents = null){
+    protected $sep = '/';
+    public function __construct(string $contents = null,string $sep = null){
+         if(null !== $sep){
+            $this->sep = $sep; 
+         }
          if(null !== $contents){
             $this($contents); 
          }
     }  
+    public function setSeparator(string $sep){
+        $this->sep = $sep; 
+        return $this;
+    }
     public function getHash(){
         return $this->hash;
     }
@@ -33,10 +40,13 @@ class XHashSha1 implements HashTypeInterface
          ];
        return $this->hash;
     }
+    public function toString(array $hash, string $sep = null): string{
+       return implode(null === $sep ? $this->sep : $sep, array_reverse($hash));
+    }
     public function __toString(): string{
        if(null === $this->getHash()){
           throw new \Exception('You must hash some contents before you can get a string!'); 
        }
-       return implode($this->sep, array_reverse($this->hash));
+       return $this->toString($this->getHash(), $this->sep);
     }
 }
