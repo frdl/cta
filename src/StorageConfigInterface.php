3cbee5e9d2d22633458031131eaf907ca20fc1e3
Till Wehowski <github@webfan.de>
2022-08-29 16:12:34 +0200

    Update and rename StorageConfigInterface.php to StorageInterface.php

--- src/StorageConfigInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Webfan\cta\Storage;
-
-interface StorageConfigInterface {
-    public function getUriStorage();
-    public function getFileStorage();
-    public function getChunkStorage();
-}
---
+++ src/StorageInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Webfan\cta\Storage;
+
+interface StorageInterface {
+
+public function serializeChunk(/* (note: any data type in standard implementation possible) */ $chunk);
+public function unserializeChunk($serialized);
+public function save(string $source,
+                     string $uri = null, 
+                     array $headers = null,
+                     bool $touch = true, /* alter FileModificationTime */
+			   bool $assoc = true,
+			   int $expiresAtTimestamp = null) : array;
+public function getHashes(
+        string $source,
+        string $uri = null,
+        int $chunksize = 80,
+        string $delimiter = null,
+        \callable|\closure $callback = null,
+        bool $assoc = true
+    ) : array;
+ public function serve(string $uri = null, $withHeaders = true) : array;
+ public function getByUri(string $uri = null, bool $verbose = false, bool $count = false, bool $withHeaders = true) : array;
+ public function getChunks(
+        string $source,
+        \callable|\closure $callback = null,
+        int $chunksize = 80,
+        string $delimiter = null
+    ) : array;
+/* public function getFileChunks( string $filename,\callable|\closure $callback = null,int $chunksize = 80,string $delimiter = null) : array; */
+}
